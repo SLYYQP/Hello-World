3d35b906fe404fc078cf875b8d0540a78b397234
SLYYQP <455576852@qq.com>
2018-11-06 10:22:49 +0800

    I have learned module.

--- DO_SEPTEMBER_2018/Lmodule.py
+++ DO_SEPTEMBER_2018/Lmodule.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+# Filename:Lmodule
 
-
+import NewModule
 
 """
 1、创建一个有一些类、方法、变量的模块
@@ -12,3 +13,10 @@
 """
 2、创建一个新的模块用来使用上一步声明的类、变量和方法
 """
+
+NewModule.fib(1000)
+NewModule.fib2(100)
+NewModule.__name__
+
+
+
---
+++ DO_SEPTEMBER_2018/NewModule.py
@@ -0,0 +1,23 @@
+# !/usr/bin/python3
+# Filename: NewModule.py
+
+
+# 斐波那契(fibonacci)数列模块
+
+def fib(n):  # 定义到 n 的斐波那契数列
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a + b
+    print()
+
+def fib2(n):  # 返回到 n 的斐波那契数列
+    re = []
+    a, b = 0, 1
+    while b < n:
+        re.append(b)
+        a, b = b, a + b
+    return re
+
+
+
